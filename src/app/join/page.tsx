6cf7d21d6e11ed4c8e7b4e13095f577ea82faf01
BEINUSH <beinushcar@gmail.com>
2026-07-23 21:50:13 +0300

    Improve navigation and room management

--- src/app/join/page.tsx
+++ src/app/join/page.tsx
@@ -1,9 +1,10 @@
 import { Suspense } from "react";
 import { JoinForm } from "./JoinForm";
+import Loading from "../loading";
 
 export default function JoinPage() {
   return (
-    <Suspense fallback={null}>
+    <Suspense fallback={<Loading />}>
       <JoinForm />
     </Suspense>
   );
